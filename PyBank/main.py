8a6578fe51b1b5283e08635838c57a9be450502a
Joel Cox <joelcox518@gmail.com>
2020-09-28 17:57:47 -0400

    Homework rough draft

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,24 +6,48 @@ import csv
 budget_data = os.path.join('Resources', 'budget_data.csv')
 #Total number of Months included in Data Set
  
-Months= ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+# Months= ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+total=0
+total_net=0
+net_change_list=[]
+greatest_increase=["",0]
+greatest_decrease=["",99999999999]
+
 
 with open(budget_data) as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
-    csv_header= next(budget_data)
-    
+    csv_header= next(csvreader)
+    jan_data= next(csvreader)
 
-    total = 0
+    total=total+1
+    total_net=total_net+int(jan_data[1])
+    previous_net=int(jan_data[1])  
 
     for row in csvreader:
-        if row[0]== Months['....']:
-            total= total + 1
-            Print (total)
+        total=total+1
+        total_net=total_net+int(row[1])
         
-         #sum of Prifit/Losses
-        print (sum[row(1)])
-         #average
-        print(average[row(1)])
+        change= int(row[1])-previous_net
+        previous_net=int(row[1])
+        net_change_list= net_change_list+ [change]
+
+        if change > greatest_increase[1]:
+            greatest_increase[1]=change
+            greatest_increase[0]= row[0]
+
+        if change < greatest_decrease[1]:
+            greatest_decrease[1]=change
+            greatest_decrease[0]= row[0]
+
+average_change= round(sum(net_change_list)/len(net_change_list),2)
+
+print(total)
+print(total_net)
+print(average_change)
+print(greatest_increase[0], greatest_increase[1])
+print(greatest_decrease[0], greatest_decrease[1])
+
+    
 
 
     
---
+++ PyPoll/Main.py
@@ -0,0 +1,36 @@
+# Pull CSV
+
+import os
+import csv
+#budget_data = os.path.join('c:', 'Users', 'joelc', 'Documents', 'python-challenge' , 'PyBank', 'Resources', 'budget_data.csv')
+election_data = os.path.join('Resources', 'election_data.csv')
+
+cand_votes= {}
+cand= []
+percent_vote=[]
+total_votes=0
+with open(election_data) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    csv_header= next(csvreader)
+
+    for row in csvreader:
+        cand=row[2]
+
+        if cand in cand_votes.keys():
+            cand_votes[cand]+=1
+        else:
+            cand_votes[cand]=1
+
+total_votes= sum(cand_votes.values())
+print(total_votes)
+for i in cand_votes:
+    percent_vote= round((float(cand_votes[i])/total_votes)*100,2)
+
+    print(f"{i} {percent_vote} {cand_votes[i]}")
+
+for key in cand_votes.keys():
+    if cand_votes[key]==max(cand_votes.values()):
+        winner=key
+
+print(winner)    
+
